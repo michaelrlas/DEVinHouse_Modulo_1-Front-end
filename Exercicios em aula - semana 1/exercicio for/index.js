cfe1faaa2a72d3e544e2ca73663b254b4b44a68b
michaelrlas <michaelsilvamkt@gmail.com>
2022-08-08 20:50:14 -0300

    semana 1

---
+++ Exercicios em aula - semana 1/exercicio for/index.js
@@ -0,0 +1,20 @@
+// let alunos = ['kaira', 'Silas', 'arthur', 'Alan'];
+
+// for (var aluno of alunos) {
+//   console.log('Aluno: ', aluno);
+// }
+
+// var contador = 1,
+//   array = [];
+
+// while (contador <= 10) {
+//   if (contador % 2 == 1) {
+//     array.push(contador);
+//   }
+//   contador++;
+// }
+
+// console.log(array);
+
+// var nome = window.prompt('Digite seu nome');
+// console.log(nome);
