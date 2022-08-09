6a8336eee80554572bbc9c300d4995e3f61615d3
michaelrlas <michaelsilvamkt@gmail.com>
2022-08-08 21:54:49 -0300

    filtrar array de objetos

---
+++ Semana 4/[M1S04] Ex 6 - order e filter/index.js
@@ -0,0 +1,27 @@
+// Dado o array de cidades abaixo, filtre apenas as cidades com mais de 200000 habitantes;
+
+// let cidades = [
+//     { nome: 'Patos de Minas', populacao: 153585 },
+//     { nome: 'Lages', populacao: 157349 },
+//     { nome: 'Ibiúna', populacao: 79479 },
+//     { nome: 'Maringá', populacao: 403063 },
+//     { nome: 'Curitiba', populacao: 1963726 },
+//     { nome: 'Florianópolis', populacao: 508826 },
+//     { nome: 'Pato Branco', populacao: 84779 },
+// ];
+// _Dica: Dar uma olhada no método filter
+
+let cidades = [
+  { nome: 'Patos de Minas', populacao: 153585 },
+  { nome: 'Lages', populacao: 157349 },
+  { nome: 'Ibiúna', populacao: 79479 },
+  { nome: 'Maringá', populacao: 403063 },
+  { nome: 'Curitiba', populacao: 1963726 },
+  { nome: 'Florianópolis', populacao: 508826 },
+  { nome: 'Pato Branco', populacao: 84779 },
+];
+
+let cidadesGrandes = cidades.filter(element => {
+  return element.populacao >= 200000;
+});
+console.log(cidadesGrandes);
