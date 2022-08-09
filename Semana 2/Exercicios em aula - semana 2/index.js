ac16c7273f305d2b468a8cb5fa9edbee0c52e29a
michaelrlas <michaelsilvamkt@gmail.com>
2022-08-08 21:00:37 -0300

    semana 2

---
+++ Semana 2/Exercicios em aula - semana 2/index.js
@@ -0,0 +1,21 @@
+//var idade = window.prompt('Digite sua idade');
+// para transformar em em numero use o parseInt
+// var idade = parseInt(window.prompt('Digite sua idade'));
+
+//document.write(idade);
+
+// var isConfirm = window.confirm();
+// document.write(isConfirm);
+// document.write(typeof isConfirm);
+//teste repositório git
+// teste
+// var nome = window.prompt('Digite seu nome');
+// var sobrenome = window.prompt('Digite seu sobrenome');
+// console.log(nome + ' ' + sobrenome);
+
+var media = 8;
+if (media >= 7) {
+  console.log(
+    'Parabéns você tirou  a média mínima, não esqueca  de continuar estudando'
+  );
+}
